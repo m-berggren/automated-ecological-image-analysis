a08ad985253f293fa6857eb7309cb9e2564f4326
Byland <byland@student.chalmers.se>
2026-04-09 13:56:05 +0200

    TG-25 Connect the backend to the frontend and modify cors settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -31,13 +31,18 @@ INSTALLED_APPS = [
     'apps.datasets',
     'rest_framework',
     'rest_framework.authtoken',
+<<<<<<< HEAD
     'corsheaders',
+=======
+    "corsheaders",
+>>>>>>> 860a74c (TG-25 Connect the backend to the frontend and modify cors settings)
 ]
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
