c148aad8c4b35c616313066841f7d4cedca284d1
skaro-dev <zaoldyeck.00@gmail.com>
2026-05-10 20:03:59 +0200

    TG-50 Experiment with 4 model training approaches
    YOLO26s-OBB with and without augmentation, and YOLO26n-OBB with and without augmentation option

--- ml-pipelines/seed_src/train.py
+++ ml-pipelines/seed_src/train.py
@@ -3,36 +3,62 @@ import os
 from ultralytics import YOLO
 
 """
-def train_species_model(species_name, data_yaml_path, epochs=200):
+Includes all 4 variations of the YOLO26-OBB model:
+- YOLO26s-OBB model without augmentations
+- YOLO26s-OBB model with augmentations
+- YOLO26n-OBB model without augmentations
+- YOLO26n-OBB model with augmentations
+The results are quite similar, so kept them all in for now.
+We can later remove the ones that are not being used.
+"""
+
+"""
+# YOLO26s-OBB model without augmentations
+def train_species_model(species_name, data_yaml_path, epochs=90):
     model = YOLO('yolo26s-obb.pt')
 
     results = model.train(
         data=data_yaml_path,
         epochs=epochs,
         imgsz=768,
-        batch=2,
+        name=species_name,  # This creates unique folders per species
+        exist_ok=True,
+        batch=-1, # Allow YOLO to decide the best batch size
+        mosaic=0.0,
+        close_mosaic=0,
+        plots=True,
+        patience=20, # Early stopping if the model doesn't improve for 20 epochs
+    )
+
+    return os.path.join('runs', 'obb', species_name, 'weights', 'best.pt')
+"""
+"""
+# YOLO26s-OBB model with augmentations
+def train_species_model(species_name, data_yaml_path, epochs=90):
+    model = YOLO('yolo26s-obb.pt')
+
+    results = model.train(
+        data=data_yaml_path,
+        epochs=epochs,
+        imgsz=768,
+        name=species_name,  # This creates unique folders per species
+        exist_ok=True,
+        batch=-1, # Allow YOLO to decide the best batch size
         mosaic=1.0,
         close_mosaic=0,
+        plots=True,
         mixup=0.2,
         degrees=45.0,
         flipud=0.5,
         fliplr=0.5,
-        augment=True,
-        plots=True,
-        exist_ok=True,
-        patience=25
+        patience=20, # Early stopping if the model doesn't improve for 20 epochs'
     )
 
-    save_dir = model.trainer.save_dir
-    best_path = os.path.join(save_dir, 'weights', 'best.pt')
-
-    print(f'{species_name} model saved at: {best_path}')
-
-    return best_path
-
+    return os.path.join('runs', 'obb', species_name, 'weights', 'best.pt')
 """
 
-
+"""
+# YOLO26n-OBB model without augmentations
 def train_species_model(species_name, data_yaml_path, epochs=90):
     model = YOLO('yolo26n-obb.pt')
 
@@ -42,11 +68,36 @@ def train_species_model(species_name, data_yaml_path, epochs=90):
         imgsz=768,
         name=species_name,  # This creates unique folders per species
         exist_ok=True,
-        batch=2,
+        batch=-1, # Allow YOLO to decide the best batch size
         mosaic=0.0,
         close_mosaic=0,
-        augment=True,
         plots=True,
+        patience=20, # Early stopping if the model doesn't improve for 20 epochs
+    )
+
+    return os.path.join('runs', 'obb', species_name, 'weights', 'best.pt')
+"""
+
+# YOLO26n-OBB model with augmentations
+def train_species_model(species_name, data_yaml_path, epochs=90):
+    model = YOLO('yolo26n-obb.pt')
+
+    results = model.train(
+        data=data_yaml_path,
+        epochs=epochs,
+        imgsz=768,
+        name=species_name,  # This creates unique folders per species
+        exist_ok=True,
+        batch=-1, # Allow YOLO to decide the best batch size
+        mosaic=1.0,
+        close_mosaic=0,
+        plots=True,
+        mixup=0.2,
+        degrees=45.0,
+        flipud=0.5,
+        fliplr=0.5,
+        patience=20, # Early stopping if the model doesn't improve for 20 epochs
+        # project='runs/obb'
     )
 
     return os.path.join('runs', 'obb', species_name, 'weights', 'best.pt')
